31df2c7057beee42d99c0c6465a6a9c224b55b86
Mani Meskartehrani <mani.tehrani.work@gmail.com>
2024-06-24 18:13:49 -0700

    Feat: Update Cancel page and success page

--- src/routes/cancel/+page.svelte
+++ src/routes/cancel/+page.svelte
@@ -1,7 +1,14 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
 <div class="container h-screen mx-auto flex justify-center items-center">
 	<div class="grid grid-cols gap-4">
 		<div class="col-span-3">
-			<h1>Sorry</h1>
+			<h1>Sorry, We miss you. Maybe next time.</h1>
 		</div>
+		<button class="btn variant-glass-secondary" on:click={() => goto('./localhost:5175')}
+			>Start Over</button
+		>
 	</div>
 </div>
--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -1,7 +1,8 @@
 <div class="container h-screen mx-auto flex justify-center items-center">
 	<div class="grid grid-cols gap-4">
 		<div class="col-span-3">
-			<h1>Success</h1>
+			<h1>Success, You make a perfect choice. See you soon.</h1>
 		</div>
+		<button class="btn variant-glass-success"></button>
 	</div>
 </div>
